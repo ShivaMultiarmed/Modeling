707e7385d3dec49520990cb4e0f59152a1cc1a64
Mikhail_Shell <theoutstandingme@gmail.com>
2025-04-23 23:32:30 +0700

    Written autocorrelation functions and displayed their hraphs

--- task5.py
+++ task5.py
@@ -1,5 +1,6 @@
 from collections.abc import Callable
 from random import random
+from typing import Tuple
 
 import numpy as np
 from matplotlib import pyplot as plt
@@ -50,8 +51,12 @@ def generate_random() -> float:
 
     return (x - x_min) / (x_max - x_min)
 
-def play_process(matrix: list[list[float]], f: Callable[[], float]) -> list[int]:
+def play_process(
+        matrix: list[list[float]],
+        f: Callable[[], float]
+) -> Tuple[list[int], list[int]]:
     states_num = len(matrix)
+    states = []
     state_counts = [0 for _ in range(states_num)]
     current_state = int(random() * states_num)
     for i in range(100_000):
@@ -62,27 +67,46 @@ def play_process(matrix: list[list[float]], f: Callable[[], float]) -> list[int]
                 new_state = j
                 break
         current_state = new_state
+        states.append(current_state)
         state_counts[current_state] += 1
 
-    return state_counts
+    return states, state_counts
+
+def show_histogram(data: list[int]) -> None:
+    plt.bar(range(1, len(data) + 1), data, alpha=0.6, color='b', edgecolor='black')
+    plt.xlabel("Номера состояний")
+    plt.ylabel("Количество переходов")
+    plt.show()
+
+
+def evaluate_auto_correlation(data: list[int]) -> list[float]:
+    data = np.asarray(data, dtype=float)
+    N = len(data)
+    avg = np.mean(data)
+    centered = data - avg
+    var = np.sum(centered ** 2)
+    return [np.sum(centered[:N-k] * centered[k:]) / var for k in range(N)]
+
+def show_auto_correlation(data: list[float]):
+    plt.stem(range(1, len(data) + 1), data) #, use_line_collection=True)
+    plt.xlabel('Лаг')
+    plt.ylabel('Автокорреляция')
+    plt.title('Автокорреляционная функция')
+    plt.grid(True)
+    plt.show()
 
 states_num = 50
-
 matrix_a = generate_matrix(states_num)
 matrix_b = generate_matrix(states_num)
 print_matrix(matrix_a)
 print_matrix(matrix_b)
-process_a = play_process(matrix_a, generate_random)
-process_b = play_process(matrix_b, generate_random)
+process_a, process_a_count = play_process(matrix_a, generate_random)
+process_b, process_b_count = play_process(matrix_b, generate_random)
 print(process_a)
 print(process_b)
-
-plt.bar(range(1, states_num + 1), process_a, alpha=0.6, color='b', edgecolor='black')
-plt.xlabel("Номера состояний")
-plt.ylabel("Количество переходов")
-plt.show()
-
-plt.bar(range(1, states_num + 1), process_b, alpha=0.6, color='b', edgecolor='black')
-plt.xlabel("Номера состояний")
-plt.ylabel("Количество переходов")
-plt.show()
+show_histogram(process_a_count)
+show_histogram(process_b_count)
+auto_correlation_a = evaluate_auto_correlation(process_a)
+auto_correlation_b = evaluate_auto_correlation(process_b)
+show_auto_correlation(auto_correlation_a[0:10])
+show_auto_correlation(auto_correlation_b[0:10])
